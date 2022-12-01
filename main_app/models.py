be656ae78425f2bd6b11afd877a606f74977f9ec
xAlice2 <helena.wu4@gmail.com>
2022-11-30 20:44:06 -0800

    feat: made model Cat

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    age = models.IntegerField()
